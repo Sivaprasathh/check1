bf403ec62d169faf13c02f5da6f0bf831fbf6ffd
Sivaprasathh <sivaprasathsekar97@gmail.com>
2021-08-15 17:55:23 +0530

    Final commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+    
+    function valueDisp() {
+        var text = document.getElementById("todo").value;
+        console.log(text);
+      document.getElementById("demo").innerHTML = text ;
+    }
+  
